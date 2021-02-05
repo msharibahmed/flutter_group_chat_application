0abb52e972a44a5f2babeeb220c0c60dec12da84
Sharib Ahmed <sharib@webmesecure.com>
2021-02-05 21:44:12 +0530

    addded logout button

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,7 +2,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-
 import 'package:flutter_group_chat_application/widgets/messages.dart';
 import 'package:flutter_group_chat_application/widgets/new_message.dart';
 
@@ -20,13 +19,38 @@ class _ChatScreenState extends State<ChatScreen> {
         appBar: AppBar(
           centerTitle: true,
           elevation: 0,
-          title: Text('Group Chat India',style: GoogleFonts.dancingScript(color: Colors.black,fontSize: 30,fontWeight: FontWeight.bold)),
+          title: Text('Group Chat India',
+              style: GoogleFonts.dancingScript(
+                  color: Colors.black,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold)),
           actions: [
-            IconButton(
-                icon: Icon(Icons.logout),
-                onPressed: () async {
-                  await FirebaseAuth.instance.signOut();
-                })
+            DropdownButton(
+              underline:Container(),
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.black,
+              ),
+              items: [
+                DropdownMenuItem(
+                  child: Container(
+                    child: Row(
+                      children: <Widget>[
+                        Icon(Icons.exit_to_app),
+                        SizedBox(width: 8),
+                        Text('Logout'),
+                      ],
+                    ),
+                  ),
+                  value: 'logout',
+                ),
+              ],
+              onChanged: (item) {
+                if (item == 'logout') {
+                  FirebaseAuth.instance.signOut();
+                }
+              },
+            ),
           ],
         ),
         body: Container(
